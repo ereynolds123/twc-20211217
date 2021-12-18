a3eb52914ae9ae97c63f8f43cf019223dde29f6e
Big Labor <biglabor22@gmail.com>
2021-12-18 12:59:36 -0800

    add requirements.txt

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ load_dotenv()
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 account_sid = os.getenv('SID')
-auth_token = os.getenv('Auth')
+auth_token = os.getenv('AUTH')
 client = Client(account_sid, auth_token)
 
 message = client.messages \
